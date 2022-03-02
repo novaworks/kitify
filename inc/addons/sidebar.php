c3823d4ff0fa2f256e25a967eb470027eb2e5a99
Novaworks <novawrs@gmail.com>
2022-03-02 14:11:39 +0700

    Update sidebar.php

--- inc/addons/sidebar.php
+++ inc/addons/sidebar.php
@@ -241,10 +241,10 @@ class Kitify_Sidebar extends Kitify_Base {
 
       echo '<div ' . $this->get_render_attribute_string( 'sidebar-wrapper' ) . '>';
         echo '<div class="kitify-toggle-sidebar__overlay js-column-toggle"></div>';
-        echo '<div class="kitify-toggle-sidebar__container nova_box_ps">';
-        echo '<a class="nova-sidebar__toggle js-sidebar-toogle" href="javascript:void(0)"></a>';
+        echo '<div class="kitify-toggle-sidebar__container">';
+        echo '<a class="nova-sidebar__toggle js-column-toggle" href="javascript:void(0)"></a>';
           echo '<div class="toggle-column-btn__wrap"><a class="toggle-column-btn js-column-toggle" href="javascript:void(0)"></a></div>';
-          echo '<div class="kitify-toggle-sidebar__inner">';
+          echo '<div class="kitify-toggle-sidebar__inner nova_box_ps">';
           dynamic_sidebar( $sidebar );
           echo '</div>';
         echo '</div>';
