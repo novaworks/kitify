11d6a2eb8d2fdeaf82b159acdb2b28b0949a5526
Novaworks <novawrs@gmail.com>
2022-03-03 16:34:25 +0700

    Update base-products-renderer.php

--- inc/modules/woocommerce/classes/base-products-renderer.php
+++ inc/modules/woocommerce/classes/base-products-renderer.php
@@ -218,8 +218,8 @@ abstract class Base_Products_Renderer extends \WC_Shortcode_Products {
         if( ! kitify()->get_theme_support('kitify-woo::product-loop') ){
 
 						add_action('woocommerce_before_shop_loop',  'kitify_setup_toolbar' , -999 );
-						add_action('woocommerce_before_shop_loop',  'vedbo_add_toolbar_open' , 15 );
-						add_action('woocommerce_before_shop_loop',  'vedbo_add_toolbar_close' , 35 );
+						add_action('woocommerce_before_shop_loop',  'kitify_add_toolbar_open' , 15 );
+						add_action('woocommerce_before_shop_loop',  'kitify_add_toolbar_close' , 35 );
 						add_action( 'nova_woocommerce_catalog_ordering', 'vedbo_add_grid_list_display', 35, 0 );
 
             add_action('kitify/products/action/shop_loop_item_button', 'woocommerce_template_loop_add_to_cart', 10);
@@ -233,7 +233,7 @@ abstract class Base_Products_Renderer extends \WC_Shortcode_Products {
             remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
 						remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
 						remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5);
-						
+
             add_action( 'woocommerce_before_shop_loop_item', [ $this, 'loop_item_open' ], -1001 );
             add_action( 'woocommerce_after_shop_loop_item', [ $this, 'loop_item_close' ], 1001 );
 
